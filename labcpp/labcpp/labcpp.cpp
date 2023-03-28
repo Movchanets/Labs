863432bf23a1c0e3dbb6519ddf4830f65acd4f70
Movchanets <slavikmovchanetc@gmail.com>
2023-03-28 12:54:45 +0300

    fix

--- labcpp/labcpp/labcpp.cpp
+++ labcpp/labcpp/labcpp.cpp
@@ -1,15 +1,18 @@
 #include "vector.h"
 
 
-void Input(Vector& vector) {
+void Input(Vector& vector)
+{
     bool isInt = false;
     int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
     std::cout << "Enter the first point\n";
 
     std::cout << "Enter x:\n";
-    while (!isInt) {
+    while (!isInt)
+    {
         isInt = static_cast<bool>(std::cin >> x1);
-        if (!isInt) {
+        if (!isInt)
+        {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cout << "Invalid input. Enter an integer value.\n";
@@ -18,9 +21,11 @@ void Input(Vector& vector) {
 
     isInt = false;
     std::cout << "Enter y:\n";
-    while (!isInt) {
+    while (!isInt)
+    {
         isInt = static_cast<bool>(std::cin >> y1);
-        if (!isInt) {
+        if (!isInt)
+        {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cout << "Invalid input. Enter an integer value.\n";
@@ -30,9 +35,11 @@ void Input(Vector& vector) {
     std::cout << "Enter the second point\n";
     std::cout << "Enter x:\n";
     isInt = false;
-    while (!isInt) {
+    while (!isInt)
+    {
         isInt = static_cast<bool>(std::cin >> x2);
-        if (!isInt) {
+        if (!isInt)
+        {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cout << "Invalid input. Enter an integer value.\n";
@@ -41,9 +48,11 @@ void Input(Vector& vector) {
 
     std::cout << "Enter y:\n";
     isInt = false;
-    while (!isInt) {
+    while (!isInt)
+    {
         isInt = static_cast<bool>(std::cin >> y2);
-        if (!isInt) {
+        if (!isInt)
+        {
             std::cin.clear();
             std::cout << "Invalid input. Enter an integer value.\n";
         }
@@ -54,57 +63,38 @@ void Input(Vector& vector) {
 }
 
 
-
-class MyClass
-{
-public:
-    void Print (std::string str)
-    {
-        std::cout << str <<std::endl;
-    }
-    int o1;
-    MyClass() { Print("Standart");  }
-    MyClass(int o1) { Print("o1");this->o1 = o1;  }
-    MyClass(const MyClass& other) {Print("const");  this->o1 = other.o1;  }
-//    MyClass(MyClass obj) {Print("obj"); this->o1 = obj.o1;  }
-};
 int main(int argc, char* argv[])
 {
-    MyClass myObj();
-  //  MyClass myObj3 = new MyClass();	
-
-
-    
-    // Vector vector;
-    // while (true)
-    // {
-    //     Input(vector);
-    //     std::cout << vector << std::endl;
-    //     std::cout << "Press any key to continue | Press Enter to exit\n";
-    //     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    //     if (std::cin.get() == '\n')
-    //     {
-    //         system("cls"); // clear the console
-    //         break;
-    //     }
-    //     system("cls");
-    // }
-    // Vector vector1(Point(1, 1), Point(2, 2));
-    // std::cout << "vector1 / double" << std::endl;
-    // vector1 = vector1 / 2.5;
-    // std::cout << vector1 << std::endl;
-    // std::cout << "double / vector" << std::endl;
-    // vector1 = 1.5 / vector1;
-    // std::cout << vector1 << std::endl;
-    // Vector vector2(Point(1, 1), Point(2, 2));
-    // std::cout << "vector1 + vector2" << std::endl;
-    // vector1 = vector1 + vector2;
-    // std::cout << vector1 << std::endl;
-    // std::cout << "vector>vector2: " << (vector1 > vector2) << std::endl;
-    // std::cout << "vector--" << std::endl;
-    // std::cout << vector2 << std::endl;
-    // std::cout << (vector2--)<< " : returned old value" << std::endl;
-    // std::cout << vector2 << " : new value" << std::endl;
-    // std::cout << "!vector :" <<std::boolalpha<< !vector2 << std::endl;
+    Vector vector;
+    while (true)
+    {
+        Input(vector);
+        std::cout << vector << std::endl;
+        std::cout << "Press any key to continue | Press Enter to exit\n";
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        if (std::cin.get() == '\n')
+        {
+            system("cls"); // clear the console
+            break;
+        }
+        system("cls");
+    }
+    Vector vector1(Point(1, 1), Point(2, 2));
+    std::cout << "vector1 / double" << std::endl;
+    vector1 = vector1 / 2.5;
+    std::cout << vector1 << std::endl;
+    std::cout << "double / vector" << std::endl;
+    vector1 = 1.5 / vector1;
+    std::cout << vector1 << std::endl;
+    Vector vector2(Point(1, 1), Point(2, 2));
+    std::cout << "vector1 + vector2" << std::endl;
+    vector1 = vector1 + vector2;
+    std::cout << vector1 << std::endl;
+    std::cout << "vector>vector2: " << (vector1 > vector2) << std::endl;
+    std::cout << "vector--" << std::endl;
+    std::cout << vector2 << std::endl;
+    std::cout << (vector2--) << " : returned old value" << std::endl;
+    std::cout << vector2 << " : new value" << std::endl;
+    std::cout << "!vector :" << std::boolalpha << !vector2 << std::endl;
     return 0;
 }
